a616a96f98a877264bee96d32e408fac8c10bb5f
Andrej <andrejjezik@gmail.com>
2020-12-07 16:00:27 +0100

    distributions implementation

--- lib/sim.cpp
+++ lib/sim.cpp
@@ -1,6 +1,11 @@
 #include "sim.h"
 
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
 namespace sim {
+
     bool Event::lessThan(double y) {
         return time <= y;
     }
@@ -23,23 +28,28 @@ namespace sim {
         return d(gen);
     }
 
-    double Random_g::normal(double m, double q){
+    double exp(double lambda){
+        double u;
         std::random_device rd;
         std::mt19937 gen(rd());
-        // values near the mean are the most likely
-        // standard deviation affects the dispersion of generated values from the mean
-        std::normal_distribution<> d{m,q};
-
-        return d(gen);
+        u = gen() / (std::mt19937::max() + 1.0);
+        return -log(1- u) / lambda;
     }
 
+    double exp_m(double mean){
+        exp(1/mean);
+    }
+
+    double Random_g::normal(double m, double q){
+         return (sqrt(-2*log(rand())) * cos(2*M_PI*rand()))*q+m;
+    }
+
+    //* uniform distribution, (0..1] */
     double Random_g::rand(){
         std::random_device rd;
         std::mt19937 gen(rd());
         //uniform between 0-1 is random
-        std::uniform_real_distribution<> d(0.0, 1.0);
-
-        return d(gen);
+        return (gen()+1.0)/(std::mt19937::max()+1.0);
     }
 
     double Random_g::uniform(double a, double b){
@@ -50,7 +60,7 @@ namespace sim {
     }
 
     int Random_g::distributions(int type){
-        const int nrolls=100000;  // number of experiments
+        const int nrolls=10000;  // number of experiments
         const int nstars=100;    // maximum number of stars to distribute
         const int nintervals=10; // number of intervals
         int p[nintervals]={};
@@ -58,27 +68,30 @@ namespace sim {
         
         if (type==1)
         {
-            std::cout << "exponential distribution exp(1)" << std::endl;
+            std::cout << "exponential distribution exp(10)" << std::endl;
+            std::map<int, int> hist{};
             for (int i=0; i<nrolls; ++i) {
-                double n =exp(3.5);
-                if (n<1.0) ++p[int(nintervals*n)];
+                double n =exp_m(10);
+                //printf("%.2f, ", n);
+                ++hist[n];
             }
-
-            for (int i=0; i<nintervals; ++i) {
-                printf("%.2f - %.2f: ",float(i)/nintervals,float(i+1)/nintervals);
-                std::cout << std::string(p[i]*nstars/nrolls,'*') << std::endl;
+            for(auto p : hist) {
+                std::cout << std::setw(2)
+                << p.first << ' ' << std::string(p.second/10, '*') << '\n';
             }
         }
         else if (type==2)
         {
             std::cout << "normal distribution normal(50,2)" << std::endl;
-             std::map<int, int> hist{};
+            std::map<int, int> hist{};
             for (int i=0; i<nrolls; ++i) {
-                ++hist[std::round(normal(5,2.5))];
+                float n = normal(10,2.5);
+                printf("%.2f, ", n);
+                ++hist[n];
             }
             for(auto p : hist) {
-            std::cout << std::setw(2)
-            << p.first << ' ' << std::string(p.second/300, '*') << '\n';
+                std::cout << std::setw(2)
+                << p.first << ' ' << std::string(p.second/30, '*') << '\n';
             }
             
         }
@@ -87,7 +100,7 @@ namespace sim {
             std::cout << "uniform distribution uniform(0,10)" << std::endl;
             std::map<int, int> hist{};
             for (int i=0; i<nrolls; ++i) {
-                ++hist[uniform(1,10)];
+                ++hist[uniform(1,10.99)];
             }
             for(auto p : hist) {
             std::cout << std::setw(2)
